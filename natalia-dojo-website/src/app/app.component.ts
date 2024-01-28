2c4e94550ed174d536cf03ac388cb996bb52a2e4
Natalia Gordon <g.nataley@gmail.com>
2024-01-28 18:10:30 +0200

    add html template

--- natalia-dojo-website/src/app/app.component.ts
+++ natalia-dojo-website/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { setTheme } from 'ngx-bootstrap/utils';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'natalia-dojo-website';
