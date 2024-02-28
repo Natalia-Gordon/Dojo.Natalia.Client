f4753de3506f2dcefd116fc85f74cbb240edb33b
Natalia Gordon <g.nataley@gmail.com>
2024-02-28 16:55:10 +0200

    update hope page

--- natalia-dojo-website/src/app/home/home.component.ts
+++ natalia-dojo-website/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import * as jQuery from "jquery";
+import { AboutComponent } from "../about/about.component";
+import { WorkoutsComponent } from "../workouts/workouts.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: []
+    imports: [AboutComponent, WorkoutsComponent]
 })
 export class HomeComponent {
 
