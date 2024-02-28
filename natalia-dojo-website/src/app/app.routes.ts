7bad2540881e7b359b4a4c8300ab2b598994e974
Natalia Gordon <g.nataley@gmail.com>
2024-02-28 12:22:44 +0200

    add workouts page

--- natalia-dojo-website/src/app/app.routes.ts
+++ natalia-dojo-website/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { TeamComponent } from './team/team.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "about", component: AboutComponent},
-    {path: "contact", component: ContactComponent}
+    {path: "contact", component: ContactComponent},
+    {path: "workouts", component: WorkoutsComponent},
+    {path: "team", component: TeamComponent}
 ];
