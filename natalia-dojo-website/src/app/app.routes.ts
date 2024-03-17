5226051bf71d275a03e14e78b537b0192efe2160
Natalia Gordon <g.nataley@gmail.com>
2024-03-17 12:03:31 +0200

    add blog and testimonials

--- natalia-dojo-website/src/app/app.routes.ts
+++ natalia-dojo-website/src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 import { TeamComponent } from './team/team.component';
+import { BlogComponent } from './blog/blog.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "about", component: AboutComponent},
     {path: "contact", component: ContactComponent},
     {path: "workouts", component: WorkoutsComponent},
-    {path: "team", component: TeamComponent}
+    {path: "team", component: TeamComponent},
+    {path: "blog", component: BlogComponent},
+    {path: "testimonial", component: TestimonialComponent}
 ];
--- natalia-dojo-website/src/app/home/home.component.ts
+++ natalia-dojo-website/src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import * as jQuery from "jquery";
 import { AboutComponent } from "../about/about.component";
 import { WorkoutsComponent } from "../workouts/workouts.component";
+import { TeamComponent } from "../team/team.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [AboutComponent, WorkoutsComponent]
+    imports: [AboutComponent, WorkoutsComponent, TeamComponent]
 })
 export class HomeComponent {
 
