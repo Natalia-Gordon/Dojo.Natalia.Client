70cece77592da62cc233962181280a9fd8a82822
Natalia Gordon <g.nataley@gmail.com>
2024-04-21 13:41:30 +0300

    migrate nav bar to angular

--- natalia-dojo-website/src/app/app.routes.ts
+++ natalia-dojo-website/src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { TestimonialComponent } from './testimonial/testimonial.component';
 import { ArticlesComponent } from './blog/articles/articles.component';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent},
+    {path: "home", component: HomeComponent},
     {path: "about", component: AboutComponent},
     {path: "contact", component: ContactComponent},
     {path: "workouts", component: WorkoutsComponent},
