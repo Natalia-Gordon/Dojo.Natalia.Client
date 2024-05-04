2277d8374c46bc5d8c0542565402ee395e84d63a
Natalia Gordon <g.nataley@gmail.com>
2024-05-04 15:17:14 +0300

    update zen-1 article

--- natalia-dojo-website/src/app/blog/articles/zen/zen-1/zen-1.component.ts
+++ natalia-dojo-website/src/app/blog/articles/zen/zen-1/zen-1.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../../../hero/hero.component";
 import { SidebarComponent } from "../../../sidebar/sidebar.component";
 import { CommentsComponent } from "../../comments/comments.component";
+import { ZenheroComponent } from "../hero/hero.component";
 
 @Component({
     selector: 'app-zen-1',
     standalone: true,
     templateUrl: './zen-1.component.html',
     styleUrl: './zen-1.component.css',
-    imports: [HeroComponent, SidebarComponent, CommentsComponent]
+    imports: [HeroComponent, SidebarComponent, CommentsComponent, ZenheroComponent]
 })
 export class Zen1Component {
 
