71a8a1550ad5a1478129f431fee5bcc62a9852aa
Natalia Gordon <g.nataley@gmail.com>
2026-01-20 13:39:28 +0200

    set home page titlt "דוג׳ו נטליה - נינג׳וטסו ואומנויות לחימה באופקים'"

--- natalia-dojo-website/src/app/home/home.component.ts
+++ natalia-dojo-website/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 import * as jQuery from "jquery";
 import { AboutComponent } from "../about/about.component";
@@ -13,7 +13,7 @@ import { RecentArticlesComponent } from "./recent-articles/recent-articles.compo
     styleUrl: './home.component.css',
     imports: [AboutComponent, WorkoutsComponent, TeamComponent, RecentArticlesComponent]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
     constructor(private title: Title, private meta: Meta) {}
     ngOnInit(): void {
         this.title.setTitle('דוג׳ו נטליה - נינג׳וטסו ואומנויות לחימה באופקים');
@@ -21,5 +21,13 @@ export class HomeComponent implements OnInit {
             name: 'description',
             content: 'דוג׳ו נטליה - בית ספר לנינג׳וטסו ואומנויות לחימה באופקים. אימוני הגנה עצמית, שיטות עבודה יפניות מסורתיות, פילוסופיה וקהילה מקצועית. הירשם לאימון ניסיון.'
         });
+    }
+    
+    ngAfterViewInit(): void {
+        // Ensure title is set after child components (like AboutComponent) initialize
+        // This prevents child components from overwriting the home page title
+        setTimeout(() => {
+            this.title.setTitle('דוג׳ו נטליה - נינג׳וטסו ואומנויות לחימה באופקים');
+        }, 0);
     }
 }
